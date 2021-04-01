5582b42b462ec08743b0efbb1d5d4e4e5e815fc3
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-01 18:47:26 +0200

    Implement Day 1.1 of AdventureOfCode

--- src/main/java/org/haffson/adventofcode/days/day01/Day01.java
+++ src/main/java/org/haffson/adventofcode/days/day01/Day01.java
@@ -4,12 +4,13 @@ import org.haffson.adventofcode.ProblemStatusEnum;
 import org.haffson.adventofcode.days.Days;
 import org.haffson.adventofcode.utils.FileReaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
 /**
- * Implementation for <i>Day 1: Chronal Calibration</i>.
+ * Implementation for <i>Day 1: Report Repair/i>.
  */
 @Component
 public class Day01 implements Days {
@@ -17,16 +18,14 @@ public class Day01 implements Days {
     /** The puzzle status {@code HashMap} */
     private final HashMap<String, ProblemStatusEnum> problemStatus;
 
-    /**
-     * Causes the input file to be parsed into the frequencies array ({@code frequencies}).
-     *
-     * @param fileReaders {@code @Autowired} fileReader //TODO: inject what you need
-     */
+    private final int[] numbers;
+
     @Autowired
-    Day01(FileReaders fileReaders) {
+    Day01(FileReaders fileReaders, @Value("${day1.file}") String filePath) {
         this.problemStatus = new HashMap<>();
         this.problemStatus.put("1", ProblemStatusEnum.UNSOLVED);
         this.problemStatus.put("2", ProblemStatusEnum.UNSOLVED);
+        this.numbers = fileReaders.readFileIntoArrayOfIntegers(filePath);
     }
 
     @Override
@@ -41,7 +40,7 @@ public class Day01 implements Days {
 
     @Override
     public String firstPart() {
-        return "Part 1 - Frequency: " + calculateFrequency();
+        return "Part 1: " + calculateReport();
     }
 
     @Override
@@ -51,11 +50,22 @@ public class Day01 implements Days {
 
     /**
      * Primary method for Day 1, Part 1.
-     * Calculates the final frequency as the sum of all frequencies.
+     * Calculates the Report
      *
-     * @return the final frequency
+     * @return Returns the product of the multiplication of the numbers that add up to 2020.
      */
-    private int calculateFrequency() {
-        return 0;
+    private int calculateReport() {
+        int sum;
+        int report = 0;
+        for(int i = 0; i < this.numbers.length - 1; i++){
+            for(int j = 1; j < this.numbers.length - 1; j++){
+                sum = numbers[i] + numbers[j];
+                if(sum == 2020){
+                    report = numbers[i] * numbers[j];
+                    break;
+                }
+            }
+        }
+        return report;
     }
 }
