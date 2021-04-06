53f16ffbac10252471f48a1986d8531f42a9c7fe
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-06 14:10:05 +0200

    Add JUnit tests for FileReaders readFileIntoArrayOfIntegers method

--- src/test/java/org/haffson/adventofcode/utils/FileReadersTest.java
+++ src/test/java/org/haffson/adventofcode/utils/FileReadersTest.java
@@ -1,18 +1,32 @@
 package org.haffson.adventofcode.utils;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class FileReadersTest {
 
-    //TODO: Meaningful tests for readFileIntoArrayOfIntegers and readFileIntoStringList
+    @Autowired
+    FileReaders fileReaders;
 
-    //TODO: not easy to test, but we should!
     @Test
-    public void openFileReturnScannerThrowsExceptionIfFileIsNotFound() {
+    public void testReadFileIntoArrayOfIntegersWithEmptyLine(){
+        String filePath = "/utilsTestFiles/fileReadersExampleInputWithEmptyLine.txt";
+        int[] expectedResult = new int[]{1721, 979, 366, 299, 675, 1456};
+        int[] actualResult = fileReaders.readFileIntoArrayOfIntegers(filePath);
+        Assert.assertArrayEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void testReadFileIntoArrayOfIntegersWithoutEmptyLine(){
+        String filePath = "/utilsTestFiles/fileReadersExampleInputWithoutEmptyLine.txt";
+        int[] expectedResult = new int[]{1721, 979, 366, 299, 675, 1456};
+        int[] actualResult = fileReaders.readFileIntoArrayOfIntegers(filePath);
+        Assert.assertArrayEquals(expectedResult, actualResult);
+    }
 }
