24ab16a6ef5a3c5c97238fa6671aae5b8e38bd11
Daniel Bruenker <Daniel.Bruenker@gmx.de>
2021-04-06 09:43:25 +0200

    Fix the error if puzzle inputs contains empty lines

--- src/main/java/org/haffson/adventofcode/utils/FileReaders.java
+++ src/main/java/org/haffson/adventofcode/utils/FileReaders.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -14,4 +13,18 @@ import java.util.Scanner;
 @Component
 public class FileReaders {
 
+    public int[] readFileIntoArrayOfIntegers(String path){
+        List<String> data = new ArrayList<>();
+        InputStream inputStream = FileReaders.class.getResourceAsStream(path);
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if(!line.isEmpty()){
+              data.add(line);
+            }
+        }
+        scanner.close();
+        return data.stream().mapToInt(Integer::parseInt).toArray();
+    }
 }
+
