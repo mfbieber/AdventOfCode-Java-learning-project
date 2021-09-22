69232b229ffa983aef339b8c41480c7085ce26ae
Jennifer Arps <jennifer@dedica.team>
2021-09-22 17:04:23 +0200

    Added Documentation to CheckStringIsEmpty.java

--- src/main/java/org/haffson/adventofcode/utils/CheckStringIsEmpty.java
+++ src/main/java/org/haffson/adventofcode/utils/CheckStringIsEmpty.java
@@ -7,6 +7,11 @@ public class CheckStringIsEmpty {
     private CheckStringIsEmpty() {
     }
 
+    /*
+    requireNonNullAndNonEmpty() checks if strings are null or empty
+    and if true throws IllegalArgumentException.
+    It helps crashing code "fail-fast" before code has already performed some side effects.
+     */
     public static String requireNonNullAndNonEmpty(String string) {
         if (StringUtils.isEmpty(string)) {
             throw new IllegalArgumentException("The string is null or empty: " + string);
