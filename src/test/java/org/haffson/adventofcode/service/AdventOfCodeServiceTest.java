c9ff0ebd964c0c8e4b71cdacb39949deea64d3d2
PascalWo <pwolff.de@gmail.com>
2022-07-07 17:37:15 +0200

    fixed serviceTest

--- src/test/java/org/haffson/adventofcode/service/AdventOfCodeServiceTest.java
+++ src/test/java/org/haffson/adventofcode/service/AdventOfCodeServiceTest.java
@@ -3,23 +3,19 @@ package org.haffson.adventofcode.service;
 import org.haffson.adventofcode.ProblemStatusEnum;
 import org.haffson.adventofcode.days.Days;
 import org.haffson.adventofcode.days.day01.Day01;
-import org.haffson.adventofcode.days.day02.Day02;
-import org.haffson.adventofcode.days.day03.Day03;
 import org.haffson.adventofcode.exceptions.PuzzleNotSolvedYetException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AdventOfCodeServiceTest {
+class AdventOfCodeServiceTest {
 
     private final List<Days> daysSolutions = new LinkedList<>();
 
@@ -29,58 +25,37 @@ public class AdventOfCodeServiceTest {
 
     @BeforeEach
     public void setup() {
-        Day01 day01 = Mockito.mock(Day01.class);
+        final Day01 day01 = Mockito.mock(Day01.class);
         problemStatus.put(1, ProblemStatusEnum.SOLVED);
         problemStatus.put(2, ProblemStatusEnum.UNSOLVED);
-
-        Day02 day02 = Mockito.mock(Day02.class);
-        Day03 day03 = Mockito.mock(Day03.class);
-        daysSolutions.add(day02);
-        daysSolutions.add(day03);
         daysSolutions.add(day01);
         Mockito.when(day01.getDay()).thenReturn(1);
-        Mockito.when(day02.getDay()).thenReturn(2);
-        Mockito.when(day03.getDay()).thenReturn(3);
-
         Mockito.when(day01.getProblemStatus()).thenReturn(problemStatus);
-        Mockito.when(day01.firstPart()).thenReturn("Product 1: " + 326211);
+        Mockito.when(day01.firstPart()).thenReturn("Part 1 - Frequency: 599");
         adventOfCodeService = new AdventOfCodeService(daysSolutions);
     }
 
     @Test
-    public void getResultsForASpecificDayAndPuzzlePartTest() {
-        String actualResult = adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(1, 1);
-        assertThat(actualResult).isEqualTo("Product 1: " + 326211);
+    void getResultsForASpecificDayAndPuzzlePartTest() {
+        final String actualResult = adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(1, 1);
+        assertEquals("Part 1 - Frequency: 599", actualResult);
     }
 
     @Test
-    public void tryingToGetResultsForANotYetImplementedPartThrowsExceptionTest() throws Exception {
-        Assertions.assertThrows(PuzzleNotSolvedYetException.class, () -> {
-            adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(1, 2);
-        });
+    void tryingToGetResultsForANotYetImplementedPartThrowsExceptionTest() {
+        Assertions.assertThrows(PuzzleNotSolvedYetException.class, () ->
+                adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(1, 2));
     }
 
     @Test
-    public void tryingToGetResultsForANotYetImplementedDayThrowsException() throws Exception {
-        Assertions.assertThrows(PuzzleNotSolvedYetException.class, () -> {
-            adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(10, 1);
-        });
+    void tryingToGetResultsForANotYetImplementedDayThrowsException() {
+        Assertions.assertThrows(PuzzleNotSolvedYetException.class, () ->
+                adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(10, 1));
     }
 
     @Test
-    public void tryingToGetResultsForAnyOtherPartThrowsException() throws Exception {
-        Assertions.assertThrows(PuzzleNotSolvedYetException.class, () -> {
-            adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(2, 3);
-        });
-    }
-
-    @Test
-    public void getSortedListOfDaysSolution() {
-        List<Integer> expectedResult = Arrays.asList(1, 2, 3);
-        final List<Integer> actualResult = adventOfCodeService.getDaysSolutions().stream()
-                .map(Days::getDay)
-                .collect(Collectors.toList());
-
-        assertThat(actualResult).isEqualTo(expectedResult);
+    void tryingToGetResultsForAnyOtherPartThrowsException() {
+        Assertions.assertThrows(PuzzleNotSolvedYetException.class, () ->
+                adventOfCodeService.getResultsForASpecificDayAndPuzzlePart(2, 3));
     }
 }
